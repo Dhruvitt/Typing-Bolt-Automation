62235dd21b317caa2e00f422268f2dc7ae05f672
Dhruvitt <pokharna.dhruvit@gmail.com>
2021-03-12 22:01:35 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Typing-Bolt-Automation
-An Automation tool for typing bolt.
+#Typing Bolt Automated Typing
+An automated typing code in python *for educational purposes only*
+
+#Installation
+Clone the Repo
+
+#Required Modules
+1. Python3
+2. run in cmd 'pip install PyAutoGUI'
+3. then install exe from - https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-v5.0.0-alpha.20201127.exe
+
+#Steps to 
+
+1. Get the Coordinates using the get_coordinates.py and put the coordinates in coordinates.py
+
+2. After putting the coordinates try running automate.py
+
+3. Viola!
+
+#Prerequisites for running
+
+*** First open a typing bolt tab and then run the automate.py ***
---
+++ automate.py
@@ -0,0 +1,50 @@
+import pyautogui as gui
+import pytesseract
+import time
+from coordinates import *
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract' 
+# change the location the the exe file of tesseract.exe accordingly
+
+Typing_Bolt = (19, 209, 1300, 40)  #for ex. (x, y, a, b)
+# where x an y is the coordinate of the top left most part of the text area and a and b are the width and length distanch from that point.
+
+def capture(region):
+    img = gui.screenshot(region=region)
+    img.save('screenshot.png')
+    return img 
+
+def predict(img):
+    text = str(pytesseract.image_to_string(img))
+    #print(text,  file=open('log.txt', 'w'))
+    return text
+
+def write_text(text):
+    gui.write(text, interval=.05) # interval = the time seperation between each letter in seconds
+
+
+time.sleep(3)
+
+gui.click(CHROME)
+time.sleep(2)
+
+gui.click(TYPING_WINDOW)
+time.sleep(1)
+
+for _ in range(RUN_N_TIMES):
+    
+    click_n_times(SCROLLER_BAR_UP)
+    time.sleep(.25)
+
+    gui.click(PRACTICE_BUTTON)
+    time.sleep(PRAC_LOAD_TIME)
+
+    img = capture(Typing_Bolt)
+    text = predict(img)
+
+    gui.click(KEYBOARD)
+    time.sleep(.25)
+
+    write_text(text)
+    time.sleep(.25)
+    
+gui.alert(f'Automation was done {RUN_N_TIMES} times!!')
